6f7ad2531ad59c04a1bfe62fa795045bedd0daaf
Ceci Cifu <ceciliocifuentes@gmail.com>
2021-04-13 23:57:59 +0200

    Unnecessary code removed & refactor

--- migrations/Version20210413215617.php
+++ migrations/Version20210413215617.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210413163339 extends AbstractMigration
+final class Version20210413215617 extends AbstractMigration
 {
     public function getDescription() : string
     {
