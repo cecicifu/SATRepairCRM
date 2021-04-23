0bbee7c9875d1a762b1f2b9fd8cbd8d133d7c33c
cecicifu <ceciliocifuentes@gmail.com>
2021-04-23 18:08:13 +0200

    Repair service

---
+++ src/Service/RepairService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\RepairRepository;
+use App\Entity\Repair;
+use Doctrine\Common\Collections\Collection;
+
+class RepairService
+{
+    private RepairRepository $repairRepository;
+
+    public function __construct(RepairRepository $repairRepository)
+    {
+        $this->repairRepository = $repairRepository;
+    }
+
+    public function findAll(): array
+    {
+        return $this->repairRepository->findAll();
+    }
+
+    public function findRepairProducts(Repair $repair): ?Collection
+    {
+        $repair = $this->repairRepository->find($repair);
+        return $repair->getProducts();
+    }
+
+    public function countRepairProducts(Repair $repair): ?int
+    {
+        $repair = $this->repairRepository->find($repair);
+        return $repair->getProducts()->count();
+    }
+
+}
