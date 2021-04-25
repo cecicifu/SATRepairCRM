4dd435fc24809d30d6d86734edde2c5885476d7c
cecicifu <ceciliocifuentes@gmail.com>
2021-04-25 13:57:20 +0200

    Routes fixed, products removed from form

--- src/Controller/Admin/ProductController.php
+++ src/Controller/Admin/ProductController.php
@@ -13,6 +13,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/product")
+ */
 class ProductController extends AbstractController
 {
     /**
