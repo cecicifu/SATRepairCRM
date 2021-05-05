5f07adc5bc3e937dc2554c3c867dad94d8129f82
cecicifu <ceciliocifuentes@gmail.com>
2021-05-05 14:11:24 +0200

    Fix route to admin dashboard

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -19,7 +19,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
          if ($this->getUser()) {
-             return $this->redirectToRoute('user_index');
+             return $this->redirectToRoute('sonata_admin_dashboard');
          }
 
         $error = $authenticationUtils->getLastAuthenticationError();
