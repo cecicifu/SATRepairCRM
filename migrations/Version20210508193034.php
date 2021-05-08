ebf94652611c66adbd1a1afb5bf7373ec26cee49
cecicifu <ceciliocifuentes@gmail.com>
2021-05-08 21:39:29 +0200

    Validations & doctrine migrations minor fixes

--- migrations/Version20210508193034.php
+++ migrations/Version20210508193034.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210430063007 extends AbstractMigration
+final class Version20210508193034 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,19 +21,19 @@ final class Version20210430063007 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE category (id CHAR(36) NOT NULL --(DC2Type:uuid)
-        , name VARCHAR(30) NOT NULL, description VARCHAR(100) DEFAULT NULL, modified DATETIME DEFAULT NULL, created DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , name CLOB NOT NULL, description CLOB DEFAULT NULL, modified DATETIME DEFAULT NULL, created DATETIME NOT NULL --(DC2Type:datetime_immutable)
         , PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE customer (id CHAR(36) NOT NULL --(DC2Type:uuid)
-        , fullname VARCHAR(70) NOT NULL, address VARCHAR(70) DEFAULT NULL, city VARCHAR(50) DEFAULT NULL, email VARCHAR(70) DEFAULT NULL, zip_code INTEGER DEFAULT NULL, phone INTEGER NOT NULL, modified DATETIME DEFAULT NULL, created DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , fullname CLOB NOT NULL, address CLOB DEFAULT NULL, city CLOB DEFAULT NULL, email CLOB DEFAULT NULL, zip_code INTEGER DEFAULT NULL, phone INTEGER NOT NULL, modified DATETIME DEFAULT NULL, created DATETIME NOT NULL --(DC2Type:datetime_immutable)
         , PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE product (id CHAR(36) NOT NULL --(DC2Type:uuid)
-        , name VARCHAR(50) NOT NULL, amount INTEGER NOT NULL, price DOUBLE PRECISION DEFAULT NULL, modified DATETIME DEFAULT NULL, created DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , name CLOB NOT NULL, amount INTEGER NOT NULL, price DOUBLE PRECISION DEFAULT NULL, modified DATETIME DEFAULT NULL, created DATETIME NOT NULL --(DC2Type:datetime_immutable)
         , PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE repair (id CHAR(36) NOT NULL --(DC2Type:uuid)
         , customer_id CHAR(36) DEFAULT NULL --(DC2Type:uuid)
         , category_id CHAR(36) DEFAULT NULL --(DC2Type:uuid)
         , status_id CHAR(36) DEFAULT NULL --(DC2Type:uuid)
-        , code VARCHAR(13) NOT NULL, imei INTEGER DEFAULT NULL, pattern VARCHAR(20) DEFAULT NULL, fault VARCHAR(85) NOT NULL, colour VARCHAR(50) DEFAULT NULL, private_comment VARCHAR(200) DEFAULT NULL, public_comment VARCHAR(200) DEFAULT NULL, labour_price DOUBLE PRECISION DEFAULT NULL, tax DOUBLE PRECISION DEFAULT NULL, visible INTEGER NOT NULL, modified DATETIME DEFAULT NULL, created DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , code VARCHAR(13) NOT NULL, imei INTEGER DEFAULT NULL, pattern CLOB DEFAULT NULL, fault CLOB NOT NULL, colour VARCHAR(7) DEFAULT NULL, private_comment CLOB DEFAULT NULL, public_comment CLOB DEFAULT NULL, labour_price DOUBLE PRECISION DEFAULT NULL, tax DOUBLE PRECISION DEFAULT NULL, visible BOOLEAN NOT NULL, modified DATETIME DEFAULT NULL, created DATETIME NOT NULL --(DC2Type:datetime_immutable)
         , PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8EE4342177153098 ON repair (code)');
         $this->addSql('CREATE INDEX IDX_8EE434219395C3F3 ON repair (customer_id)');
@@ -47,11 +47,11 @@ final class Version20210430063007 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_DA2223F18EE43421 ON repair_products (repair)');
         $this->addSql('CREATE INDEX IDX_DA2223F1D34A04AD ON repair_products (product)');
         $this->addSql('CREATE TABLE status (id CHAR(36) NOT NULL --(DC2Type:uuid)
-        , name VARCHAR(25) NOT NULL, colour VARCHAR(50) DEFAULT NULL, modified DATETIME DEFAULT NULL, created DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , name CLOB NOT NULL, colour VARCHAR(7) DEFAULT NULL, modified DATETIME DEFAULT NULL, created DATETIME NOT NULL --(DC2Type:datetime_immutable)
         , PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_7B00651C5E237E06 ON status (name)');
         $this->addSql('CREATE TABLE user (id CHAR(36) NOT NULL --(DC2Type:uuid)
-        , username VARCHAR(15) NOT NULL, email VARCHAR(70) DEFAULT NULL, password VARCHAR(100) NOT NULL, last_session DATETIME DEFAULT NULL, modified DATETIME DEFAULT NULL, created DATETIME NOT NULL --(DC2Type:datetime_immutable)
+        , username CLOB NOT NULL, email CLOB DEFAULT NULL, password CLOB NOT NULL, last_session DATETIME DEFAULT NULL, modified DATETIME DEFAULT NULL, created DATETIME NOT NULL --(DC2Type:datetime_immutable)
         , PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649F85E0677 ON user (username)');
     }
