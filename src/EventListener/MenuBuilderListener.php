3ced1b1f289f34a8267b1e3c3562b49c423c4149
cecicifu <ceciliocifuentes@gmail.com>
2021-05-13 09:43:41 +0200

    Dashboard route in the menu

--- src/EventListener/MenuBuilderListener.php
+++ src/EventListener/MenuBuilderListener.php
@@ -10,7 +10,14 @@ final class MenuBuilderListener
     {
         $menu = $event->getMenu();
 
-        $menu->addChild('reports', [
+        $menu->addChild('dashboard', [
+            'label' => 'Dashboard',
+            'route' => 'sonata_admin_dashboard',
+        ])->setExtras([
+            'icon' => '<i class="fas fa-tachometer-alt"></i>',
+        ]);
+
+        $menu->addChild('logout', [
             'label' => 'Logout',
             'route' => 'app_logout',
         ])->setExtras([
