8c1ee2c0cb474315dde012853b6e2a8234453c67
cecicifu <ceciliocifuentes@gmail.com>
2021-05-14 21:24:39 +0200

    Separated tracker css & js files & minor format refactor

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('tracker', './assets/tracker.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
