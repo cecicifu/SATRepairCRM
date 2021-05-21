e19fcff2a5f951b09824af36fb7e8c5c1723da01
cecicifu <ceciliocifuentes@gmail.com>
2021-05-21 12:49:30 +0200

    Crontab command repairs
    Hide repairs where created date is greater than 90 days

---
+++ src/Command/BatchRepairHideCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Command;
+
+use App\Service\RepairService;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class BatchRepairHideCommand extends Command
+{
+    protected static string $defaultName = 'batch:repair:hide';
+    protected static string $defaultDescription = 'Add a short description for your command';
+
+    private RepairService $repairService;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager, RepairService $repairService)
+    {
+        parent::__construct();
+        $this->repairService = $repairService;
+        $this->entityManager = $entityManager;
+    }
+
+    protected function configure(): void
+    {
+        $this->setDescription(self::$defaultDescription);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        $repairs = $this->repairService->findAll();
+        $totalRepairs = 0;
+        foreach ($repairs as $repair) {
+            $interval = $repair->getCreated()->diff(new DateTime('now'));
+            $days = (int) $interval->format("%a");
+
+            if($days >= 90 && $repair->isVisible()) {
+                $repair->setVisible(false);
+                $this->entityManager->flush();
+                $totalRepairs++;
+            }
+        }
+
+        $io->success(sprintf('Repairs hidden: %s', $totalRepairs));
+
+        return Command::SUCCESS;
+    }
+}
