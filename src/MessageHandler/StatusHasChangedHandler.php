79f91b853c4c173e5fd2e1e634e2a030b394a691
cecicifu <ceciliocifuentes@gmail.com>
2021-05-27 19:09:26 +0200

    Template created & minor refactor

--- src/MessageHandler/StatusHasChangedHandler.php
+++ src/MessageHandler/StatusHasChangedHandler.php
@@ -3,6 +3,8 @@
 namespace App\MessageHandler;
 
 use App\Message\StatusHasChanged;
+use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
@@ -11,34 +13,35 @@ use Symfony\Component\Mime\Email;
 
 final class StatusHasChangedHandler implements MessageHandlerInterface
 {
+    private LoggerInterface $logger;
     private MailerInterface $mailer;
 
-    public function __construct(MailerInterface $mailer)
+    public function __construct(LoggerInterface $logger, MailerInterface $mailer)
     {
+        $this->logger = $logger;
         $this->mailer = $mailer;
     }
 
-    /**
-     * @throws TransportExceptionInterface
-     */
     public function __invoke(StatusHasChanged $message): void
     {
-        $email = (new Email())
-            ->from(Address::create('SATRepairCRM <status@satrepaircrm.me>'))
-            ->to($message->getEmail())
-            ->priority(Email::PRIORITY_NORMAL)
-            ->subject("Repair Status Update {$message->getCode()}")
-            ->html("
-				Your repair status ({$message->getCode()}) has been updated to <b>{$message->getStatus()}.</b>
-				<i>This e-mail is an automatic confirmation. Please do not reply to it.</i>
-			")
-        ;
+        try {
+            $email = (new TemplatedEmail())
+                ->from(Address::create('SATRepairCRM <status@satrepaircrm.me>'))
+                ->to($message->getEmail())
+                ->priority(Email::PRIORITY_NORMAL)
+                ->subject("Repair status updated {$message->getCode()}")
+                ->htmlTemplate('email/status_has_changed.html.twig')
+                ->context(['message' => $message])
+            ;
 
-        $email
-            ->getHeaders()
-            ->addTextHeader('X-Auto-Response-Suppress', 'All')
-        ;
+            $email
+                ->getHeaders()
+                ->addTextHeader('X-Auto-Response-Suppress', 'All')
+            ;
 
-        $this->mailer->send($email);
+            $this->mailer->send($email);
+        } catch (TransportExceptionInterface $e) {
+            $this->logger->error($e);
+        }
     }
 }
